1f22c612e1318149bca3bcabaa6aafe4619cfc38
Domonique Griffin <griffind2009@gmail.com>
2016-09-17 23:30:57 -0400

    created outlines

---
+++ main.js
@@ -0,0 +1,42 @@
+/*<div id="text"></div>
+<button>next</button>*/
+/*var texts = [
+  'one',
+  'two',
+  'three'
+]
+var index = 0
+$(text).html(texts[index])
+$("button").on("click", function(){
+  index++
+  if(index === texts.length){
+    index = 0
+  }
+  $(text).html(texts[index])
+})*/
+window.onload = function (){
+var alphabet = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i,', 'j', 'k', 'l', 'm', 'n', 'o',
+  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+var guess;
+var guesses;
+var word;
+var space;
+
+for (i = 0; i < alphabet.length; i++)
+var index = 0
+alphabet[i];
+/*var canvas = document.getElementById("canvas")
+function {
+  Stickman= document.getElementById("Stickman");
+  context = canvas.getContext('2d');
+  context.beginpath();
+  context.strokestyle = rgb(255,255,255);
+  context.lineWidth = 3;
+  context.arc(60, 25, 10, 0, Math.PI*2, true);
+  context.stroke();
+}*/
+if guess === true ('You win')
+guess.addEventListener("click", aplhabet);
+}
