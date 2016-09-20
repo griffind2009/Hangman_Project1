f3f1edeb7db610941a83b08bcd04efb868d77abb
Domonique Griffin <griffind2009@gmail.com>
2016-09-20 01:37:43 -0400

    cleaned up js

--- main.js
+++ main.js
@@ -7,19 +7,22 @@ var alphabet = [
 
 var guess;
 var guesses = alphabet;
-var word;
-var space = "_";
-var correctGuess;
+var word = '';
+//var space;
+//var correctGuess;
+var totalLives = 4;
+var head;
+var body;
+var arms;
+var legs;
+var canvasArray = [];
 //var getWord = $(".word .input").html();
 
 
 
 //word = document.querySelector('input').innerHTML = "text"
-/*var canvasArray = [head, body, arms, legs];
-var animate = function () {
-  canvasArray();
-}
-var word = function () {
+
+/*var word = function () {
   for (i=0; i<word.length; i++) {
     document.querySelector('input').innerHTML = "text"
   }
@@ -29,81 +32,99 @@ var word = function () {
 var guessButton = $("#Guesses").children("input.guess")
 var wordButton = $("#hiddenWord").children("input.word")
 var guessButton = $("#Guesses" .input)
-guessButton.on("click", function () {
-  var guesses = ("#Guesses .input").text()
-  console.log(guesses)
-  myGuess()
-});
-var wordButton = $("#hiddenWord" .input)
-wordButton.on("click", function () {
-  var word = ("#hiddenWord .input").text()
-  console.log(word)
-  myGuess()
-});
 
-function myGuess () {
+
+/*function myGuess () {
   for (i = 0; i < alphabet.length; i++) {
-    var guesses = alphabet[i];
-    if (guesses === true) {
+    var guesses = indexOf(alphabet.length);
+    if (guesses === word) {
       return guesses
     }
   }
-}
+}*/
 
-// var getWord = $(".word .input").html();
-function convertLettersToUnderscores(word){
- var underscores = ''
-  for(var i = 0; i < word.length; i++){
-   underscores += "_"
-  }
-  return underscores
-}
 
 // grab the user's input
 var word = prompt("Please enter a word").toLowerCase();
 
 // convert the word to all underscores
 var convertedWord = convertLettersToUnderscores(word)
-
+function convertLettersToUnderscores(word){
+  var word = $(".word").html(convertedWord);
+ var underscores = ''
+  for(var i = 0; i < word.length; i++){
+   underscores += "_"
+  }
+  return convertedWord;
+}
 // player2 enters a letter to guess
  function guessALetter(){
+   //var guess = $(".input .guess").html();
   var guess = prompt("Guess a letter").toLowerCase()
   // if the guessed letter is in the original word
     var isLetterInWord = word.includes(guess)
     console.log(isLetterInWord);
     // reveal the letter in the convertedWord
-    if (isLetterInWord === true){
-      return (guess, convertedWord);
-      console.log(guess, convertedWord)
-      alert("what is guess?!")
+    if (isLetterInWord === word.includes(guess)){
+      return $('#guess').val('');
     }
   // otherwise, mark as an incorrect guess
   else {
-    canvasArray()
+
+    return newLives();
   }
     // draw another portion of the stick figure
 }
-
 guessALetter()
 
+function repeatGuesses() {
 
+}
 
+function newLives() {
+    var livesRemaining = totalLives,
+            string = word.toLowerCase();
 
+    for (var i = 0; i < guesses.length; i++) {
+        if (string.indexOf(guesses[i], 0) == -1) {
+            livesRemaining-=1;
+            console.log(livesRemaining)
+        }
+    }
 
+ if (livesRemaining <= 0) {
+   showLives.innerHTML = "Game Over";
+ }
+ for (var i = 0; i < geusses.length; i++) {
+   if (convertedWord === word) {
+     showLives.innerHTML = "You Win!";
+   }
+ }
+}
+guessButton.on("click", function () {
+  var guesses = ("#Guesses .input").text()
+  console.log(guesses)
+  guessALetter()
+});
+var wordButton = $("#hiddenWord" .input)
+wordButton.on("click", function () {
+  var word = ("#hiddenWord .input").text()
+  console.log(word)
+  guessALetter()
+});
 
-
-
-
+var canvasArray = [head, body, arms, legs]
 var canvas = document.getElementById("Stickman");
 if (canvas.getContext("2d")) {
 context = canvas.getContext("2d");
 
+var head =
 context.beginPath();
 context.fillStyle = "white";
 context.arc(200, 50, 30, 0, Math.PI * 2, true);
 context.fill();
 
-
+var head =
 context.beginPath();
 context.strokeStyle = "white";
 context.lineWidth = 3;
@@ -111,7 +132,7 @@ context.arc(200, 50, 20, 0, Math.PI, false);
 context.stroke();
 
 // body
-
+var body =
 context.beginPath();
 context.moveTo(200, 80);
 context.lineTo(200, 180);
@@ -120,7 +141,7 @@ context.stroke();
 
 
 // arms
-
+var arms =
 context.beginPath();
 context.strokeStyle = "white";
 context.moveTo(200, 80);
@@ -131,7 +152,7 @@ context.stroke();
 
 
 // legs
-
+var legs =
 context.beginPath();
 context.strokeStyle = "white";
 context.moveTo(200, 180);
@@ -139,7 +160,8 @@ context.lineTo(150, 280);
 context.moveTo(200, 180);
 context.lineTo(250, 280);
 context.stroke();
-}
+    }
+
 // help from http://www.kodyaz.com/html5/draw-stick-man-in-html5-canvas-using-javascript.aspx
 
 
@@ -194,21 +216,4 @@ var buttons.append(letters).eq()
 var letters.append(list)
 var myButtons.appendChild(letters);
 }
-}*/
-
-
-//var index = 0
-//alphabet[i];
-/*var canvas = document.getElementById("canvas")
-function {
-  Stickman= document.getElementById("Stickman");
-  context = canvas.getContext('2d');
-  context.beginpath();
-  context.strokestyle = rgb(255,255,255);
-  context.lineWidth = 3;
-  context.arc(60, 25, 10, 0, Math.PI*2, true);
-  context.stroke();
-}*/
-/*if guess === true ('You win')
-guess.addEventListener("click", aplhabet);
 }*/
